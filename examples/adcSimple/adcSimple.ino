e98ce9c98ad3afea5e212a40aaa46915f77bacbc
krywenko <skrywenko@gmail.com>
2017-04-23 10:18:40 -0500

    Add files via upload

---
+++ examples/adcSimple/adcSimple.ino
@@ -0,0 +1,47 @@
+/*
+  Example for MCP3008 - Library for communicating with MCP3008 Analog to digital converter.
+  Created by Uros Petrevski, Nodesign.net 2013
+  Released into the public domain.
+*/
+
+#include <MCP3008s.h>
+
+
+// define pin connections
+#define CS_PIN4 7
+#define CS_PIN3 8
+#define CS_PIN2 6
+#define CS_PIN 10
+#define CLOCK_PIN 13
+#define MOSI_PIN 11
+#define MISO_PIN 12
+
+// put pins inside MCP3008 constructor
+MCP3008 adc(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN, CS_PIN2,  CS_PIN3, CS_PIN4 );
+
+void setup() {
+ 
+ // open serial port
+ Serial.begin(9600);
+  
+}
+
+
+void loop() {
+  
+  
+  int val = adc.readADC(0); // read Chanel 0 from MCP3008 ADC
+  Serial.println(val);
+  
+  
+  // iterate thru all channels
+  /*
+  for (int i=0; i<8; i++) {
+   int val = adc.readADC(i);
+   Serial.print(val);
+   Serial.print("\t");
+   }
+   Serial.println("");
+  */
+  
+}
